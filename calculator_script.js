84f35ad130b5017ecd3a09ed63ed301c3ebebbd9
Oscar <ortizj2289@yahoo.com>
2024-09-27 17:39:03 -0500

    deleted the operator_set flag since it wasn't doing anything, and added functionality to the percentage operator. The percent button works now

--- calculator_script.js
+++ calculator_script.js
@@ -2,22 +2,26 @@ let operand1 = 0;
 let operand2 = 0;
 let operator = "";
 let result = 0;
-let operator_set = false;
 
 function click_function(val){
     let display = document.getElementById("digital_display");
 
-    if(!isNaN(val)){    //display the number the user clicked
+    if(!isNaN(val)){    //display the NUMBER the user clicked
         display.textContent += val;
-    } else if(val != "="){ //display the operator the user clicked...
+    } else if(val != "="){ // DO (or DONT) display the OPERATOR the user clicked...
         
-        if(display.textContent === ""){ //UNLESS the user has not clicked a number yet!
+        if(display.textContent === ""){ //DONT display if the display is empty
             alert("invalid format used.");
-        }else{
+        }else{  //DO display if the display is not empty
             operator = val; //identifies the operator used
-            operator_set = true;
             operand1 = display.textContent;   //sets the first value
             display.textContent += val;   //display the operator
+
+            //Calculate PERCENTAGE
+            if(val === "%"){
+                operand1 = Number(operand1) / 100;
+                display.textContent = operand1;
+            }
         }
 
     } else if(val == "="){  //perform the calculation and display the result
