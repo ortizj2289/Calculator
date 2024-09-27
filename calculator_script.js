a33cc3c039cd4da9972a8a566d8f93c6676dd653
Oscar <ortizj2289@yahoo.com>
2024-09-27 16:24:37 -0500

    the logic that was meant to prevent zooming in has been removed since it did not work.

--- calculator_script.js
+++ calculator_script.js
@@ -4,23 +4,6 @@ let operator = "";
 let result = 0;
 let operator_set = false;
 
-/////////////////////////////////////////////////////////////////////////////////////////
-//prevent double-tap zoom-in
-let lastTouchTime = 0; // Variable to store the last touch timestamp
-const rapidTouchThreshold = 300; // Threshold in milliseconds
-
-document.addEventListener('touchstart', function(event) {
-    const currentTime = new Date().getTime(); // Get current timestamp
-
-    // Check if the time since last touch is less than the threshold
-    if (currentTime - lastTouchTime < rapidTouchThreshold) {
-        event.preventDefault(); // Prevent default action if it's a rapid touch
-    }
-
-    lastTouchTime = currentTime; // Update last touch time
-}, { passive: false });
-//////////////////////////////////////////////////////////////////////////////////////////
-
 function click_function(val){
     let display = document.getElementById("digital_display");
 
